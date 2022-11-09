4fe8acdf9295a99014e5e6fd3fa5fa1532cb31f3
Svedin <anton.svedin@sogeti.com>
2022-11-09 14:14:01 +0100

    f

---
+++ f_structs_traits/target/debug/deps/f_structs_traits.d
@@ -0,0 +1,5 @@
+C:\Users\asvedin\dev\rust-tutorial\ultimate_rust_crash_course\exercise\f_structs_traits\target\debug\deps\f_structs_traits.exe: src\main.rs
+
+C:\Users\asvedin\dev\rust-tutorial\ultimate_rust_crash_course\exercise\f_structs_traits\target\debug\deps\f_structs_traits.d: src\main.rs
+
+src\main.rs:
---
+++ f_structs_traits/target/debug/f_structs_traits.d
@@ -0,0 +1,1 @@
+C:\Users\asvedin\dev\rust-tutorial\ultimate_rust_crash_course\exercise\f_structs_traits\target\debug\f_structs_traits.exe: C:\Users\asvedin\dev\rust-tutorial\ultimate_rust_crash_course\exercise\f_structs_traits\src\main.rs
